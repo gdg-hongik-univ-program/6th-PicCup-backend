8215e9e8fb6a291bc7b36caadd3631fe324035fe
C311122 <c311122@g.hongik.ac.kr>
2026-07-08 21:20:44 +0900

    feat: ERD기반 엔티티 + Repository 추가

---
+++ src/main/java/com/piccup/backend/entity/Category.java
@@ -0,0 +1,21 @@
+package com.piccup.backend.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "category")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name; // 카테고리명
+}
---
+++ src/main/java/com/piccup/backend/repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.piccup.backend.repository;
+
+import com.piccup.backend.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
