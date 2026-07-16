af7adc507a077e4d2bfa84ccdb726ebe22abbbce
chjpooh2002 <chjpooh2002@naver.com>
2026-07-16 20:17:05 +0900

    feat: S3 presigned URL 생성 메서드 추가

--- src/main/java/com/piccup/backend/service/S3Uploader.java
+++ src/main/java/com/piccup/backend/service/S3Uploader.java
@@ -5,9 +5,13 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
 
 import java.io.IOException;
+import java.time.Duration;
 import java.util.UUID;
 
 @Component
@@ -15,6 +19,7 @@ import java.util.UUID;
 public class S3Uploader {
 
     private final S3Client s3Client;
+    private final S3Presigner s3Presigner;
 
     // yml의 버킷 이름을 읽어옴
     @org.springframework.beans.factory.annotation.Value("${cloud.aws.s3.bucket}")
@@ -51,5 +56,22 @@ public class S3Uploader {
                 .format(java.time.format.DateTimeFormatter.ofPattern("yyyyMM"));
         String uuid = UUID.randomUUID().toString();
         return "best-picks/" + userId + "/" + yearMonth + "/" + uuid + ".jpg";
+    }
+
+    // presigned GET URL 생성 (조회용, 10분 유효)
+    public String generatePresignedUrl(String key) {
+        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                .bucket(bucket)
+                .key(key)
+                .build();
+
+        GetObjectPresignRequest presignRequest = GetObjectPresignRequest.builder()
+                .signatureDuration(Duration.ofMinutes(10))
+                .getObjectRequest(getObjectRequest)
+                .build();
+
+        return s3Presigner.presignGetObject(presignRequest)
+                .url()
+                .toString();
     }
 }
