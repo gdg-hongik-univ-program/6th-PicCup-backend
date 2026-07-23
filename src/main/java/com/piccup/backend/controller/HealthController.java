6ee5069c5029e06afead2166a77ac6f00fb20912
C311122 <c311122@g.hongik.ac.kr>
2026-07-23 18:40:31 +0900

    feat: send query to Aiven

--- src/main/java/com/piccup/backend/controller/HealthController.java
+++ src/main/java/com/piccup/backend/controller/HealthController.java
@@ -1,10 +1,23 @@
 package com.piccup.backend.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class HealthController {
-    @GetMapping("/health")
-    public String health() { return "OK"; }
+
+    // DB에 쿼리를 날리기 위함
+    private final JdbcTemplate jdbcTemplate;
+
+    @GetMapping("/api/health")
+    public ResponseEntity<String> healthCheck() {
+        // 더미 쿼리를 실행
+        jdbcTemplate.queryForObject("SELECT 1", Integer.class);
+
+        return ResponseEntity.ok("Backend and DB are alive!");
+    }
 }
