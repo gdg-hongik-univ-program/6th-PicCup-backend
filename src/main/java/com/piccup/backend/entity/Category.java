2fc41e3f93e26ecad7133c8bedc4430d8997a965
C311122 <c311122@g.hongik.ac.kr>
2026-07-31 12:21:41 +0900

    feat: categories 초안 작성

--- src/main/java/com/piccup/backend/entity/Category.java
+++ src/main/java/com/piccup/backend/entity/Category.java
@@ -4,12 +4,12 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
 import java.time.LocalDateTime;
 
 @Entity
 @Table(
         name = "category",
+        // 같은 유저(user_id)는 똑같은 이름(name)의 카테고리를 중복해서 만들 수 없다.
         uniqueConstraints = @UniqueConstraint(
                 name = "uq_category_user_name",
                 columnNames = {"user_id", "name"}
@@ -23,7 +23,6 @@ public class Category {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // 6주차 전까지 nullable
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
@@ -31,19 +30,31 @@ public class Category {
     @Column(nullable = false, length = 50)
     private String name;
 
+    @Column(name = "is_default", nullable = false)
+    private boolean isDefault; // true면 미분류
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt; // 소프트 삭제 여부
+
     @Column(name = "created_at", updatable = false)
     private LocalDateTime createdAt;
 
+    // 자동 실행 메서드
     @PrePersist
     protected void onCreate() {
         this.createdAt = LocalDateTime.now();
     }
 
     // 정적 팩토리 메서드
-    public static Category createCategory(User user, String name) {
+    public static Category createCategory(User user, String name, boolean isDefault) {
         Category category = new Category();
         category.user = user;
         category.name = name;
+        category.isDefault = isDefault;
         return category;
+    }
+
+    public void updateName(String name) {
+        this.name = name;
     }
 }
